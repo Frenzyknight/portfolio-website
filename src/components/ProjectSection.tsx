608eb9b3a92c397441c4f10b4dca9fd4dfed9b4a
Gaurav Ganju <gauravganju@gmail.com>
2025-03-26 01:22:13 +0530

    added &apos;

--- src/components/ProjectSection.tsx
+++ src/components/ProjectSection.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import { IconLink } from "@tabler/icons-react";
 export default  function ProjectSection() {
     return (
         <section className="flex bg-black flex-col gap-12 w-full py-10 px-6 md:py-14 lg:px-8 xl:px-12 font-sora items-center">
@@ -19,8 +18,8 @@ export default  function ProjectSection() {
                         <h2 className="text-4xl md:text-5xl lg:text-6xl font-bold mb-4">01</h2>
                         <h3 className="text-2xl lg:text-3xl xl:text- font-bold mb-4">Crypto Screener Application</h3>
                         <p className="text-zinc text-base mb-6">
-                        I'm Evren Shah Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-                        been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of
+                        I&apos;m Evren Shah Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+                        been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of
                         type and scrambled it to specimen book.
                         </p>
                     </div>
@@ -41,8 +40,8 @@ export default  function ProjectSection() {
                         <h2 className="text-4xl md:text-5xl lg:text-6xl font-bold mb-4">02</h2>
                         <h3 className="text-2xl lg:text-3xl xl:text- font-bold mb-4">Crypto Screener Application</h3>
                         <p className="text-zinc text-base mb-6">
-                        I'm Evren Shah Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-                        been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of
+                        I&apos;m Evren Shah Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+                        been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of
                         type and scrambled it to specimen book.
                         </p>
                     </div>
@@ -63,8 +62,8 @@ export default  function ProjectSection() {
                         <h2 className="text-4xl md:text-5xl lg:text-6xl font-bold mb-4">03</h2>
                         <h3 className="text-2xl lg:text-3xl xl:text- font-bold mb-4">Crypto Screener Application</h3>
                         <p className="text-zinc text-base mb-6">
-                        I'm Evren Shah Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-                        been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of
+                        I&apos;m Evren Shah Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+                        been the industry&apos;s standard dummy text ever since the 1500s, when an unknown printer took a galley of
                         type and scrambled it to specimen book.
                         </p>
                     </div>
