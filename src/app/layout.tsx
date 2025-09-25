fc37fcfca028de68b77d4ba99fb7f96fa4b065c6
Gaurav Ganju <gauravganju@gmail.com>
2025-09-25 16:36:59 +0530

    unused import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
-import SplashCursor from "@/components/ui/splashCursor";
 import Footer from "@/components/Footer";
 import { SpeedInsights } from "@vercel/speed-insights/next"
 const geistSans = Geist({
